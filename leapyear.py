14f169e1ae1bb3e1ea8a8f4874f887c613963fe1
Pamilerin Simon <125550439+pamilerinsimon03@users.noreply.github.com>
2024-07-23 23:56:31 +0100

    Add files via upload
    Leap Year Checker

---
+++ leapyear.py
@@ -0,0 +1,10 @@
+print("This is a program to check if a year is a leap year")
+year = int(input("Enter a year: "))
+if (year % 4 == 0):
+    if (year % 100 == 0):
+        if (year % 400 == 0):
+            print(f"The year {year} is a leap year")
+        else:
+            print(f"The year {year} is not a leap year")
+    else:
+        print(f"The year {year} is a leap year")
